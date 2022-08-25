8d8ab7d93136f001336e3a592d3000af5d001e2e
lokky19 <thankgod.agodi@gmail.com>
2022-08-26 10:23:36 +1300

    Hidden Discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
